716efcd55755c5534c18b07fcba3b57ecb0cdd3b
tobia <tobia_e.svensson@hotmail.com>
2024-02-07 20:32:32 +0100

    added stylesheet

---
+++ CarRentalRazor/Pages/UserManager/Create.cshtml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using CarRentalRazor.Data;
+using CarRentalRazor.Models;
+
+namespace CarRentalRazor.Pages.UserManager
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ICustomer customerRepository;
+
+        public CreateModel(ICustomer customerRepository)
+        {
+            this.customerRepository = customerRepository;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Customer Customer { get; set; } = default!;
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            // Exclude specific properties from validation
+            ModelState.Remove("Customer.Reservations");
+
+            if (!ModelState.IsValid || customerRepository == null || Customer == null)
+            {
+                return Page();
+            }
+
+            customerRepository.Add(Customer);
+
+            return RedirectToPage("./SignIn");
+        }
+    }
+}
